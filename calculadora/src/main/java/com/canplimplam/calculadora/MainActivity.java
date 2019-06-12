1b686986bd653e525f25b4e87175cf0060bcf8de
plamena29 <plamena.bojkova@gmail.com>
2019-06-12 10:10:13 +0200

    cambios varios en Calculadora

--- calculadora/src/main/java/com/canplimplam/calculadora/MainActivity.java
+++ calculadora/src/main/java/com/canplimplam/calculadora/MainActivity.java
@@ -2,7 +2,9 @@ package com.canplimplam.calculadora;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,5 +21,34 @@ public class MainActivity extends AppCompatActivity {
         int numero = Integer.parseInt(strNumero);
     }
 
+    public void botonPulsado(View view){
+        int cifra = 0;
+        String accion = "";
 
+        switch(view.getId()){
+
+            case R.id.id0:      cifra = 0;
+                                break;
+            case R.id.id1:      cifra = 1;
+                                break;
+            case R.id.id2:      cifra = 2;
+                                break;
+            case R.id.id3:      cifra = 3;
+                                break;
+            case R.id.id4:      cifra = 4;
+                                break;
+            case R.id.id5:      cifra = 5;
+                                break;
+            case R.id.id6:      cifra = 6;
+                                break;
+            case R.id.id7:      cifra = 7;
+                                break;
+            case R.id.id8:      cifra = 8;
+                                break;
+            case R.id.id9:      cifra = 9;
+                                break;
+        }
+
+       // Toast.makeText(getApplicationContext(),"Pulsado:" + cifra, Toast.LENGTH_LONG).show();
+    }
 }
